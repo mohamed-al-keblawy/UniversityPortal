6382247efcb99fc57f5f15a3bc87f6732445401a
Mohamed Al Keblawy <alkeblawy.m@gmail.com>
2025-06-09 08:48:49 +0400

    try azure from vs

--- UniversityPortal.Web/Program.cs
+++ UniversityPortal.Web/Program.cs
@@ -3,8 +3,26 @@ using UniversityPortal.BLL.Interfaces;
 using UniversityPortal.BLL.Services;
 using UniversityPortal.DAL.Interfaces;
 using UniversityPortal.DAL.Repositories;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (Uri.TryCreate(builder.Configuration["AppConfig"], UriKind.Absolute, out var endpoint))
+{
+    // Use Azure Active Directory authentication.
+    // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
+    // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(endpoint, new DefaultAzureCredential())
+        .ConfigureRefresh(refresh =>
+        {
+            // All configuration values will be refreshed if the sentinel key changes.
+            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+        });
+    });
+}
+builder.Services.AddAzureAppConfiguration();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -42,6 +60,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseAzureAppConfiguration();
 app.UseRouting();
 
 app.UseAuthentication();
